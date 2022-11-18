293c955d217a408e9ff2145b3f8eba82f5e2bd44
ukiyun <byoa91@gmail.com>
2022-11-18 12:19:33 +0000

    Added code to povoar.sql; added new dependencies and added 2 new topics to relatório

--- 1ª Entrega/SQL/povoar.sql
+++ 1ª Entrega/SQL/povoar.sql
@@ -13,3 +13,115 @@ INSERT INTO Equipa (nome, classificação) VALUES ('AD Valongo', 11);
 INSERT INTO Equipa (nome, classificação) VALUES ('CD Paços de Arcos', 12);
 INSERT INTO Equipa (nome, classificação) VALUES ('Parede FC', 13);
 INSERT INTO Equipa (nome, classificação) VALUES ('GRF Murches', 14);
+
+-- Data : Jogador
+-- SL Benfica
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Pedro Henriques", "27-09-1990")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Bernardo Mendes", "12-03-1999")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Diogo Rafael", "17-11-1989")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Poka", "07-05-1989")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Edu Lamas", "16-12-1990")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Nil Roca", "28-09-1997")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Carlos Nicolía", "04-04-1986")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Lucas Ordóñez", "21-04-1988")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Gonçalo Pinto", "14-02-1997")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Pablo Álvarez", "30-09-1986")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Roberto Di Benedetto", "08-05-1997")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Pol Manrubia", "06-07-2000")
+
+-- Sporting CP
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Ângelo Girão", "28-08-1989")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Zé Diogo Macedo", "17-07-1993")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Matías Platero", "17-10-1988")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Gonzalo Romero", "09-09-1992")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Henrique Magalhães", "22-10-1991")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("João Almeida", "07-04-1995")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Ferran Font", "12-11-1996")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Alessandro Verona", "11-08-1995")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("João Souto", "26-07-1992")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Toni Pérez", "07-06-1990")
+
+-- OC Barcelos
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Conti Acevedo", "24-06-1997")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Joka", "21-11-1990")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Zé Pedro", "30-03-1990")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Luís Querido", "22-08-1990")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("André Centeno", "22-03-1986")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Miguel Vieira", "05-09-1996")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Darío Giménez", "28-02-1987")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Danilo Rampulla", "11-03-1999")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Miguel Rocha", "05-01-1993")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Alvarinho", "18-04-1996")
+
+-- FC Porto
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Xavi Malián", "07-09-1989")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Tiago Rodrigues", "17-08-1998")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Telmo Pinto", "23-01-1993")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Xavi Barroso", "14-11-1992")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Reinaldo Garcia", "15-01-1983")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Diogo Barata", "24-04-2002")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Ezequiel Mena", "03-07-1998")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Rafa", "10-11-1991")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Carlo di Benedetto", "31-05-1996")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Roc Pujadas", "25-12-2001")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Gonçalo Alves", "26-07-1993")
+
+-- SC Tomar
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Albert Mola", "17-04-1997")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("António Marante", "00-00-2002")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Tomás Moreira", "19-02-1997")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Filipe Almeida", "00-00-1991")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Pedro Martins", "18-05-1995")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Guilherme Silva", "24-08-1994")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Lucas Honório", "05-05-2003")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Ivo Silva", "20-06-1990")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Franco Feruccio", "24-11-1997")
+
+-- Famalicense
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Ricardo Silva", "10-03-1983")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Manuel Silva", "00-00-2002")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Juan López", "21-07-1986")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Gabi Silva", "20-11-1995")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Carlos Dias", "14-01-2002")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("João Lima", "27-02-1999")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Thomas Molina", "23-08-2003")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Joca Guimarães", "10-01-1996")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Hugo Costa", "05-05-1988")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Rafael Almeida", "00-00-2002")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Tomás Castanheira", "26-03-1992")
+
+-- Juventude de Viana
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Carlitos", "23-07-1997")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Bruno Guia", "20-02-1998")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Rémi Herman", "04-10-1997")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("João Pedro", "09-04-1999")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Nanu Castro", "14-10-2001")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Rúben Sousa", "02-06-1992")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Andrés Castaño", "17-07-2001")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Pedro Delgado", "18-12-1993")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Diogo Casanova", "23-01-1996")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Pedro Batista", "09-09-1997")
+
+-- HC Braga
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Tomás Teixeira", "07-05-2004")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Nélson Filipe", "05-11-1984")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("António Trabulo", "25-02-1999")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Gonçalo Meira", "19-05-1998")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Diogo Seixas", "04-12-1996")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Pedro Mendes", "04-01-1993")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Tomas Korosec", "30-03-1998")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Tiago Almeida", "23-08-1998")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Vítor Hugo", "23-10-1984")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Rúben Pereira", "09-05-1990")
+
+-- Riba D'Ave
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Tomás Teixeira", "07-05-2004")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Nélson Filipe", "05-11-1984")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("António Trabulo", "25-02-1999")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Gonçalo Meira", "19-05-1998")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Diogo Seixas", "04-12-1996")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Pedro Mendes", "04-01-1993")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Tomas Korosec", "30-03-1998")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Tiago Almeida", "23-08-1998")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Vítor Hugo", "23-10-1984")
+INSERT INTO Jogador (nome, dataNasc) VALUES ("Rúben Pereira", "09-05-1990")
