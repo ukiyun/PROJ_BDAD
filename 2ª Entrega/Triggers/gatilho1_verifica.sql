c3b900c78888f42faaab695c17ef925ee772a9f6
ukiyun <byoa91@gmail.com>
2022-12-21 19:49:00 +0000

    Added new triggers

--- 2ª Entrega/Triggers/gatilho1_verifica.sql
+++ 2ª Entrega/Triggers/gatilho1_verifica.sql
@@ -7,9 +7,18 @@ PRAGMA foreign_keys = ON;
 
 /* verifica se gatilho 1 funciona */
 
-INSERT INTO Equipa (idEquipa, nome, classificação) VALUES (1, 'Portimonense', 1);
+-- Inserir novas Equipas
+
+INSERT INTO Equipa (idEquipa, nome, classificação) VALUES (15, 'Portimonense', 15);
+INSERT INTO Equipa (idEquipa, nome, classificação) VALUES (16, 'Belenenses', 16);
+INSERT INTO Equipa (idEquipa, nome, classificação) VALUES (17, 'Vitória de Guimarães', 17);
+INSERT INTO Equipa (idEquipa, nome, classificação) VALUES (18, 'Vitória de Setúbal', 18);
+INSERT INTO Equipa (idEquipa, nome, classificação) VALUES (19, 'Rio Ave', 19);
+
+-- Inserir Equipas Inválidas
 INSERT INTO Equipa (idEquipa, nome, classificação) VALUES (2, NULL, 2);
 INSERT INTO Equipa (idEquipa, nome, classificação) VALUES (3, '', 3);
+
 
 SELECT * FROM Equipa;
 
--- 2ª Entrega/Triggers/gatilho2_verifica.sql
+++ 2ª Entrega/Triggers/gatilho2_verifica.sql
@@ -7,8 +7,20 @@ PRAGMA foreign_keys = ON;
 
 /* verifica se gatilho 2 funciona */
 
-INSERT INTO Golo (idGolo, minuto, jogador, jogo, equipa)  VALUES (383, 17, 150, 58, 14);
-INSERT INTO Golo (idGolo, minuto, jogador, jogo, equipa)  VALUES (384, -5, 150, 58, 14);
-INSERT INTO Golo (idGolo, minuto, jogador, jogo, equipa)  VALUES (385, 70, 150, 58, 14);
+-- Inserir novos Golos
+
+INSERT INTO Jogo (idJogo, dataHora, resultado, jornada) VALUES (64, "09-12-2022 21:30:00", "4-1", 10);
+INSERT INTO Golo (idGolo, minuto, jogador, jogo, equipa)  VALUES (434, 2, 137, 64, 13);
+INSERT INTO Golo (idGolo, minuto, jogador, jogo, equipa)  VALUES (435, 14, 135, 64, 13);
+INSERT INTO Golo (idGolo, minuto, jogador, jogo, equipa)  VALUES (436, 19, 150, 64, 14);
+INSERT INTO Golo (idGolo, minuto, jogador, jogo, equipa)  VALUES (437, 34, 134, 64, 13);
+INSERT INTO Golo (idGolo, minuto, jogador, jogo, equipa)  VALUES (438, 41, 135, 64, 13);
+
+
+
+
+--Inserir Golos Inválidos
+INSERT INTO Golo (idGolo, minuto, jogador, jogo, equipa)  VALUES (439, -5, 149, 64, 14);
+INSERT INTO Golo (idGolo, minuto, jogador, jogo, equipa)  VALUES (439, 70, 150, 64, 14);
 
 SELECT * FROM Golo;
